e792a82de101c988c7db3de72d34482bc172357d
Oleg Hailenko <oleg.hailenko@pragmasoft.com.ua>
2018-08-09 18:38:28 +0300

    Commit for trigger build

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,6 +14,6 @@ public class DemoApplication {
     
     @RequestMapping("/")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello");
+        return ResponseEntity.ok("Hello word");
     }
 }
